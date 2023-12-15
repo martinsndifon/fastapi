7b3324cab47b972f03c2d459d4f5278ee1d2bfa9
martinsndifon <martinsndifon@gmail.com>
2023-12-15 21:12:00 +0100

    Admin router update and update to main

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -4,7 +4,7 @@ Main entry to the application
 from fastapi import FastAPI
 from database import engine
 from models import Base
-from routers import auth, todos
+from routers import auth, todos, admin, users
 
 app = FastAPI()
 
@@ -12,3 +12,5 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(users.router)
+app.include_router(admin.router)
---
+++ TodoApp/routers/admin.py
@@ -0,0 +1,122 @@
+"""
+Main entry to the application
+"""
+from typing import Annotated
+from pydantic import BaseModel, Field
+
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, Path
+from starlette import status
+
+from database import SessionLocal
+from models import Todos
+from .auth import get_current_user
+
+
+router = APIRouter(prefix="/admin", tags=["admin"])
+
+
+def get_db():
+    """Database dependency"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+class TodoRequest(BaseModel):
+    """Provides data validation using pydantic BaseModel"""
+
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
+@router.get("/todos", status_code=status.HTTP_200_OK)
+async def read_all(user: user_dependency, db: db_dependency):
+    """Return all todos from the database"""
+    if user is None or user.get("user_role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
+        )
+    return db.query(Todos).all()
+
+
+@router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(
+    user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)
+):
+    """Return a single todo based on the id"""
+    if user is None or user.get("user_role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
+        )
+
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@router.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(
+    user: user_dependency, db: db_dependency, todo_request: TodoRequest
+):
+    """Create a new todo in the db"""
+    if user is None or user.get("user_role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
+        )
+    todo_model = Todos(**todo_request.model_dump(), owner_id=user.get("id"))
+
+    db.add(todo_model)
+    db.commit()
+
+
+@router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(
+    user: user_dependency,
+    db: db_dependency,
+    todo_request: TodoRequest,
+    todo_id: int = Path(gt=0),
+):
+    """Update a todo in the db"""
+    if user is None or user.get("user_role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
+        )
+
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found.")
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
+
+
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(
+    user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)
+):
+    """Delete a todo from the db"""
+    if user is None or user.get("user_role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
+        )
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if not todo_model:
+        raise HTTPException(status_code=404, detail="Todo not found")
+
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
