5ea55d9c26eabfd08b87bafab0006adebef37da0
martinsndifon <martinsndifon@gmail.com>
2023-12-20 19:37:51 +0100

    completed the todos endpoints

--- TodoAppFrontend/routers/todos.py
+++ TodoAppFrontend/routers/todos.py
@@ -2,9 +2,12 @@ import sys
 
 sys.path.append("..")
 
-from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter, Request
-import models
+from starlette import status
+from starlette.responses import RedirectResponse
+
+from typing import Annotated, Optional
+from fastapi import Depends, HTTPException, APIRouter, Request, Form
+from models import Base, Todos
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
@@ -18,7 +21,7 @@ router = APIRouter(
     prefix="/todos", tags=["todos"], responses={404: {"description": "Not found"}}
 )
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -31,19 +34,102 @@ def get_db():
         db.close()
 
 
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
 @router.get("/", response_class=HTMLResponse)
-async def read_all_by_user(request: Request):
+async def read_all_by_user(request: Request, db: db_dependency):
     """Return all todos for a user"""
-    return templates.TemplateResponse("home.html", {"request": request})
+    todos = db.query(Todos).filter(Todos.owner_id == 1).all()
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_new_todo(request: Request):
-    """Add a new todo in the db"""
+    """Return the form for adding new todo"""
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(
+    request: Request,
+    db: db_dependency,
+    title: str = Form(...),
+    description: str = Form(...),
+    priority: int = Form(...),
+):
+    """Add new todo in the db"""
+    todo_model = Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
-async def edit_todo(request: Request):
-    """Edit a todo"""
-    return templates.TemplateResponse("edit-todo.html", {"request": request})
+async def edit_todo(request: Request, db: db_dependency, todo_id: int):
+    """Return the edit todo form"""
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
+    return templates.TemplateResponse(
+        "edit-todo.html", {"request": request, "todo": todo}
+    )
+
+
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(
+    request: Request,
+    db: db_dependency,
+    todo_id: int,
+    title: str = Form(...),
+    description: str = Form(...),
+    priority: int = Form(...),
+):
+    """Edit a todo in the database"""
+    todo = (
+        db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == 1).first()
+    )
+
+    todo.title = title
+    todo.description = description
+    todo.priority = priority
+
+    db.add(todo)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+
+@router.get("/delete/{todo_id}")
+async def delete_todo(request: Request, db: db_dependency, todo_id: int):
+    """Delete a todo from the db"""
+    todo = (
+        db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == 1).first()
+    )
+
+    if todo is None:
+        return RedirectResponse(url="/todos", status_code=status.HTTP_404_NOT_FOUND)
+
+    db.delete(todo)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+
+@router.get("/complete/{todo_id}", response_class=HTMLResponse)
+async def complete_todo(request: Request, db: db_dependency, todo_id: int):
+    """Mark a todo as complete"""
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    todo.complete = not todo.complete
+
+    db.add(todo)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
